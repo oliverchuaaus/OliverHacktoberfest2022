c7d2eb28d07466e164fe0b644af876429c499c33
kcochua <44495798+kcochua@users.noreply.github.com>
2022-10-28 23:42:24 +1100

    Add files via upload

---
+++ Occupation.java
@@ -0,0 +1,26 @@
+package com.tougher.app.v1.model.ref;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "Occupation")
+@Data
+public class Occupation {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+	@GenericGenerator(name = "native", strategy = "native")
+	private Long id;
+
+	@Column(length = 20)
+	private String occupation;
+}
